09fe036cd88c9b811e58a4838557d38c090aad83
Petr Brantalík <grounipteam@gmail.com>
2022-02-18 08:27:37 +0100

    example WIP

--- react-rtc-example/src/components/Chat.tsx
+++ react-rtc-example/src/components/Chat.tsx
@@ -1,44 +1,44 @@
 import { useEffect, useState } from 'react';
-// import { useChat } from 'react-rtc';
+import { useChat } from '@torolocos/react-rtc/src/context/Chat'; //TODO: fix path
 
 import './styles.css';
 
 const Chat = () => {
-  // const { onSend, onEnterChat, onLeaveChat, state, messageData, error } =
-  //   useChat();
-  // const [inputValue, setInputValue] = useState('');
-  // const [chatOpen, setChatOpen] = useState(false);
-  // const { isEntered } = state;
+  const { onSend, onEnterChat, onLeaveChat, state, messageData, error } =
+    useChat();
+  const [inputValue, setInputValue] = useState('');
+  const [chatOpen, setChatOpen] = useState(false);
+  const { isEntered } = state;
 
-  // useEffect(() => {
-  //   return () => {
-  //     onLeaveChat();
-  //   };
-  // }, []);
+  useEffect(() => {
+    return () => {
+      onLeaveChat();
+    };
+  }, []);
 
-  // const onStartChat = () => {
-  //   onEnterChat({ name: 'Placeholder', avatar: '' });
-  //   console.log('onSTART CHAT');
-  //   //TODO: zkontrolovat jestli pripojeni probehlo uspesne
-  //   setChatOpen(true);
-  // };
+  const onStartChat = () => {
+    onEnterChat({ name: 'Placeholder', avatar: '' });
+    console.log('onSTART CHAT');
+    //TODO: zkontrolovat jestli pripojeni probehlo uspesne
+    setChatOpen(true);
+  };
 
-  // const onEndChat = () => {
-  //   onLeaveChat();
-  //   //TODO: zkontrolovat jestli pripojeni probehlo uspesne
-  //   setChatOpen(false);
-  // };
+  const onEndChat = () => {
+    onLeaveChat();
+    //TODO: zkontrolovat jestli pripojeni probehlo uspesne
+    setChatOpen(false);
+  };
 
-  // const onMessageSend = () => {
-  //   onSend(inputValue);
+  const onMessageSend = () => {
+    onSend(inputValue);
 
-  //   setInputValue('');
-  // };
+    setInputValue('');
+  };
 
   return (
     <div>
       <h2>Chat</h2>
-      {/* {error && <div className='errorText'>Something went wrong</div>}
+      {error && <div className='errorText'>Something went wrong</div>}
       <div>
         {messageData.map(({ message, username }) => (
           <div>
@@ -57,7 +57,7 @@ const Chat = () => {
       )}
       <button onClick={!isEntered ? onStartChat : onEndChat}>
         {!isEntered ? 'join' : 'leave chat'}
-      </button> */}
+      </button>
     </div>
   );
 };
--- react-rtc-example/src/index.tsx
+++ react-rtc-example/src/index.tsx
@@ -1,18 +1,11 @@
 import ReactDOM from 'react-dom';
-import { ChatProvider } from 'react-rtc';
+import { ChatProvider } from '@torolocos/react-rtc/src/context/Chat'; //TODO: fix path
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  // <ChatProvider
-  //   signalingServer='ws://3.71.110.139:8001/'
-  //   iceServers={[{ urls: 'stun:stun.l.google.com:19302' }]}
-  // >
-  //   <App />
-  // </ChatProvider>,
   <ChatProvider
-    signalingServer='ws://3.71.110.139:8001/'
+    signalingServer='ws://3.71.110.139:8001/' // TODO: hide this. Maybe create fetch request on BE
     iceServers={[{ urls: 'stun:stun.l.google.com:19302' }]}
   >
     <App />
@@ -23,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
