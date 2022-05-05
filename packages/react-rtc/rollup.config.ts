6784085af575a08935e73e52c99f120a2709c728
pchalupa <chalupa.petr93@gmail.com>
2022-05-05 14:41:23 +0200

    chore: add rollup config

---
+++ packages/react-rtc/rollup.config.ts
@@ -0,0 +1,29 @@
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    plugins: [typescript()],
+    input: './src/index.tsx',
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/esm/index.mjs',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  },
+  {
+    plugins: [dts()],
+    input: './src/index.tsx',
+    output: {
+      file: 'dist/types/index.d.ts',
+      format: 'es',
+    },
+  },
+];
