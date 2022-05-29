77348127cf0e6aa25f9a19649b420a8c2bb88b49
pchalupa <chalupa.petr93@gmail.com>
2022-05-29 07:56:37 +0200

    refactor: remove default values

--- packages/react-rtc/src/RtcContext.ts
+++ packages/react-rtc/src/RtcContext.ts
@@ -1,11 +1,4 @@
 import { createContext } from 'react';
 import type { ContextType } from './types';
 
-export const RtcContext = createContext<ContextType>({
-  send: () => undefined,
-  disconnect: () => undefined,
-  enter: () => undefined,
-  state: { isEntered: false },
-  onMessage: () => undefined,
-  onSend: () => undefined,
-});
+export const RtcContext = createContext<ContextType>({});
