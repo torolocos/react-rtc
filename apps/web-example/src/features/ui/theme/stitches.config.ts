3a417b28947fd9541626fc31a2dd3f70e6d1e4f6
Petr Brantalík <grounipteam@gmail.com>
2022-08-02 17:20:59 +0200

    feat: added colors and fonts to the theme

--- apps/web-example/src/features/ui/theme/stitches.config.ts
+++ apps/web-example/src/features/ui/theme/stitches.config.ts
@@ -3,17 +3,29 @@ import { createStitches } from '@stitches/react';
 const stitches = createStitches({
   theme: {
     fonts: {
-      system: 'system-ui',
+      MaisonNeue:
+        '"Maison Neue", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+      Rift: 'Rift, system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+      monospace: '"Maison Neue Mono", "Consolas", "SF Mono", monospace',
+      // aliases
+      body: '$MaisonNeue',
+      title: '$Rift',
     },
     colors: {
-      hiContrast: 'hsl(206,10%,5%)',
-      loContrast: 'white',
+      primary: '#286dfe',
+      secondary: '#202329',
+      tertiary: '#2a9ef4',
+      text: '#8d8e94',
+      title: '#e6e8ea',
+      body: '#131517',
+      danger: '#cf4342',
+      success: '#57a77d',
     },
-    fontSizes: {
-      1: '13px',
-      2: '15px',
-      3: '17px',
-    },
+    // fontSizes: {
+    //   1: '13px',
+    //   2: '15px',
+    //   3: '17px',
+    // },
   },
 });
 
