35508e3958b2b1d1f4d87b598ed3009043b35cad
pchalupa <chalupa.petr93@gmail.com>
2022-08-08 13:48:19 +0200

    chore: update relase script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,13 +5,14 @@ VERSION=`auto version`
 ## Support for label 'skip-release'
 if [ ! -z "$VERSION" ]; then
   ## Publish Package
-  npm version $VERSION --workspace=@torolocos/react-rtc
+  npm version $VERSION --git-tag-version=false --workspace=@torolocos/react-rtc
   npm publish --access public --workspace=@torolocos/react-rtc
 
   ## Update dependencies
   yarn workspace example upgrade @torolocos/react-rtc
 
   git add **/package.json
+  git commit -m "chore: release"
 
   ## Create GitHub Release
   git push --follow-tags --set-upstream origin $branch
