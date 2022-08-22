c5e8b96acfa8885c69ccdec3155c9ba015b64631
pchalupa <chalupa.petr93@gmail.com>
2022-08-22 20:54:33 +0200

    test: add tests

---
+++ packages/signaling-server/src/__tests__/send.test.ts
@@ -0,0 +1,31 @@
+import { OPEN, type WebSocket } from 'ws';
+import { sendToAllClients, sendToClient } from '../send';
+
+describe('send', () => {
+  const send = jest.fn();
+  const client = jest.fn(
+    () =>
+      ({
+        readyState: OPEN,
+        send,
+      } as unknown as WebSocket)
+  );
+  const clients = new Set<WebSocket>();
+  const message = 'test';
+
+  beforeAll(() => {
+    clients.add(client());
+  });
+
+  it('should send message to client', () => {
+    sendToClient(client(), message);
+
+    expect(send).toBeCalledWith(message);
+  });
+
+  it('should send message to all clients', () => {
+    sendToAllClients(clients, message);
+
+    expect(send).toBeCalledWith(message);
+  });
+});
