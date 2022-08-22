8f35697c7d6e152fef7c131f7ab22f4d017fc7e2
pchalupa <chalupa.petr93@gmail.com>
2022-08-22 19:44:57 +0200

    refactor: pull send out

--- packages/signaling-server/src/index.ts
+++ packages/signaling-server/src/index.ts
@@ -1,22 +1,27 @@
-import * as WebSocket from 'ws';
+import {
+  Server,
+  OPEN as WebSocketOpen,
+  type RawData,
+  type WebSocket,
+} from 'ws';
 import type { Server as HttpServer } from 'http';
 import type { Server as HttpsServer } from 'https';
 
+const sendToAllClients = (message: string, webSocket: Server<WebSocket>) => {
+  webSocket.clients.forEach((client) => {
+    if (client.readyState === WebSocketOpen) client.send(message);
+  });
+};
+
 export const createSignalingServer = (server: HttpServer | HttpsServer) => {
-  const webSocket = new WebSocket.Server({ server });
+  const webSocket = new Server({ server });
 
-  const sendToAllClients = (message: string) => {
-    webSocket.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) client.send(message);
-    });
-  };
-
-  const handleMessage = (message: WebSocket.RawData) =>
-    sendToAllClients(message.toString('utf-8'));
+  const handleMessage = (message: RawData) =>
+    sendToAllClients(message.toString('utf-8'), webSocket);
 
   const handleError = () => webSocket.close();
 
-  const handleConnection = (socket: WebSocket.WebSocket) => {
+  const handleConnection = (socket: WebSocket) => {
     socket.on('message', handleMessage);
     socket.on('error', handleError);
   };
