dd7968129d5035ee732325e300ffadee7178a631
pchalupa <chalupa.petr93@gmail.com>
2022-09-02 12:01:06 +0200

    fix: username generator

--- apps/example/src/utils.ts
+++ apps/example/src/utils.ts
@@ -1,14 +1,16 @@
 export function generateUserName() {
-  const string = crypto.randomUUID().match(/[a-z]/gm);
-  const name = filterDuplicatedCharacters(string).slice(0, 6);
+  const string = crypto.randomUUID().match(/[a-z]/gm)?.join('');
+  if (string) {
+    const name = filterDuplicatedCharacters(string);
 
-  return toUpperCaseFirstCharacter(name);
+    return toUpperCaseFirstCharacter(name);
+  }
 }
 
-export function toUpperCaseFirstCharacter(text: string) {
-  text.charAt(0).toUpperCase() + text.slice(1);
+export function toUpperCaseFirstCharacter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function filterDuplicatedCharacters(text?: string[] | null) {
-  return Array.from(new Set(text)).join('');
+export function filterDuplicatedCharacters(string: string) {
+  return Array.from(new Set(string)).join('');
 }
